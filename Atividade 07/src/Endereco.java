494acfdfbd80a4ad5a45bf1b676db0a4c6a9f83e
Victor M. Vieira <53177932+vmvuno@users.noreply.github.com>
2024-04-19 16:21:33 -0300

    Atividade 07 - Adiciona Classe Endereco

---
+++ Atividade 07/src/Endereco.java
@@ -0,0 +1,8 @@
+public class Endereco {
+    private TipoEndereco tipo;
+    private Bairro bairro;
+    private Logradouro logradouro;
+    private Integer numero;
+    private String complemento;
+    private Integer CEP;
+}
